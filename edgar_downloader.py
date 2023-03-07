467212465b74bfb062d057bb97025f148a7b88fd
Athul Parvelikudy <athulparvelikudy@kubrickgroup.com>
2023-03-07 17:53:58 +0000

    Write page first try

--- edgar_downloader.py
+++ edgar_downloader.py
@@ -1,4 +1,15 @@
 #Athul
+import requests
+
+def write_page(url, file_path):
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        with open(file_path, 'w') as f:
+            f.write(response)
+        print(f"HTML file written to {file_path}")
+    else:
+        print("Failed to write HTML file")
 
 
 #Will
