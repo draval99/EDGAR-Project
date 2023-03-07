f53ed44c6a1be0c7ab97b47878c330235b6a1ab0
William Renouf <williamrenouf@kubrickgroup.com>
2023-03-07 17:11:24 +0000

    shadow code

--- edgar_downloader.py
+++ edgar_downloader.py
@@ -2,3 +2,13 @@
 
 
 #Will
+
+def download_files_10k(ticker : str, destination_folder : str):
+    '''
+    Downloads the all the 10-k files for the given ticker and puts them into
+    the destination folder path.
+    '''
+
+    if index == '10-k'
+         write_file(f,destination_folder)
+    
