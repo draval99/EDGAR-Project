3e80a95f4ba1c9539dd5b16f4a26b7ad510f1e3f
Athul Parvelikudy <athulparvelikudy@kubrickgroup.com>
2023-03-08 20:59:09 +0000

    Reading files variation  Athul

--- edgar_sentiment_wordcount.py
+++ edgar_sentiment_wordcount.py
@@ -18,4 +18,30 @@ def write_document_sentiments(input_folder, output_file):
 
     file.close()
 
-     
+#athul
+import os
+from collections import Counter
+
+def write_document_sentiments(input_folder, output_file):
+    # Define a dictionary to store the sentiment counts
+    sentiment_counts = {
+        'positive': 0,
+        'negative': 0,
+        'neutral': 0
+    }
+
+    # Loop through each file in the input folder
+    for filename in os.listdir(input_folder):
+        # Read the file
+        with open(os.path.join(input_folder, filename), 'r') as f:
+            text = f.read()
+
+        # Count the number of words in the document belonging to each sentiment
+        positive_count = text.count('positive_sentiment_word')
+        negative_count = text.count('negative_sentiment_word')
+        neutral_count = text.count('neutral_sentiment_word')
+
+        # Add the counts to the sentiment_counts dictionary
+        sentiment_counts['positive'] += positive_count
+        sentiment_counts['negative'] += negative_count
+        sentiment_counts['neutral'] += neutral_count
