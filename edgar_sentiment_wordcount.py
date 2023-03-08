058946f430c50b2bfa2c8b2eabbbcdd03d9827b1
William Renouf <williamrenouf@kubrickgroup.com>
2023-03-08 20:38:59 +0000

    Changed output file to use pandas

--- edgar_sentiment_wordcount.py
+++ edgar_sentiment_wordcount.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 
 def write_document_sentiments(input_folder, output_file):
     '''
@@ -10,12 +11,14 @@ def write_document_sentiments(input_folder, output_file):
     for file in input_folder:
         sentiment_data = file.get_sentiment_word_dict() # Part 3C function - Outputs dictionary of word sentiments
         data.append(sentiment_data)
+    
+    with open(output_file, 'w', encoding = 'UTF8', newline = '') as file:
+        df = pd.DataFrame.from_dict(data)
+        file_name = output_file.split('_')
+        df.insert(0, 'filing_date', file_name[2], inplace = True)
+        df.insert(0, 'report_type', file_name[1], inplace = True)
+        df.insert(0, 'symbol', file_name[0], inplace = True)
 
-    with open(output_file, 'w') as file:
-        writer = csv.writer(file)
-        for year in data:
-            writer.writerow(year)
-
-    file.close()
+    
 
      
