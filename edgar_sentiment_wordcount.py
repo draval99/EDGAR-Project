04ea3a3acecd9d83db8bd67d6f7ce0c724cbc6ff
William Renouf <williamrenouf@kubrickgroup.com>
2023-03-08 17:30:10 +0000

    write document sentiments first draft

--- edgar_sentiment_wordcount.py
+++ edgar_sentiment_wordcount.py
@@ -1,2 +1,21 @@
+import csv
+
 def write_document_sentiments(input_folder, output_file):
-    pass
+    '''
+    Takes a folder with cleaned html files and outputs a
+    dataframe of word sentiments for each year
+    '''
+
+    data = []
+    for file in input_folder:
+        sentiment_data = file.get_sentiment_word_dict() # Part 3C function - Outputs dictionary of word sentiments
+        data.append(sentiment_data)
+
+    with open(output_file, 'w') as file:
+        writer = csv.writer(file)
+        for year in data:
+            writer.writerow(year)
+
+    file.close()
+
+     
