e2a6c6d43f1060ec0ae6f36a1060e2aed1bb4e86
SubinSunny <subinsunny@kubrickgroup.com>
2023-03-09 12:11:43 +0000

    part c

--- edgar_reference_data.py
+++ edgar_reference_data.py
@@ -17,3 +17,26 @@ def get_sp100():
         tickers.append(ticker)
 
     return tickers
+
+def get_sentiment_word_dict():
+    import pandas as pd
+    df = pd.read_csv('Loughran-McDonald_MasterDictionary_1993-2021 (1).csv')
+    df = df.drop(['Seq_num', 'Word Count', 'Word Proportion', 'Average Proportion', 'Std Dev', 'Doc Count', 'Syllables', 'Source'], axis=1)
+    
+    
+    negative_list =[]    
+    positive_list = []
+    uncertainty_list = []
+    for i in range(len(df)):
+        if df['Negative'][i] != 0:
+            negative_list.append(df['Word'][i])
+        elif df['Positive'][i] != 0:
+            positive_list.append(df['Word'][i])
+        elif df['Uncertainty'][i] != 0:
+            uncertainty_list.append(df['Word'][i])
+
+
+
+    my_dict = {'Negative': negative_list, 'Positive': positive_list, 'Uncertainty': uncertainty_list}
+
+    return my_dict
