109fff5df3684747cab4b666c8385b9cb8e589ce
Oli Tribe <olitribe@kubrickgroup.com>
2023-03-09 09:21:25 +0000

    adding os import

--- edgar_cleaner.py
+++ edgar_cleaner.py
@@ -3,6 +3,12 @@
 
 import requests
 from bs4 import BeautifulSoup
+import re
+import os
+import edgar_downloader
+
+
+
 
 def clean_html_text(html_text):
 
@@ -10,7 +16,6 @@ def clean_html_text(html_text):
     text = soup.get_text()
 
 
-    import re
     clean_text = re.sub('[^A-Za-z0-9]+', ' ', text)
     print(clean_text)
 
@@ -27,13 +32,26 @@ def write_clean_html_text_files(input_folder, dest_folder):
     # Loop through the file names
 
     for file_name in file_names:
+        
         my_file = open(file_name,'r')
 
-        html_text = myfile.read()
+        html_text = my_file.read()
 
         clean_file = clean_html_text(html_text)
 
+        #<ticker>_10-k_<filing_date>.txt.
 
-        file_path = os.path.join(destination_folder, file_name)
+        new_file_name = file_name.remove('html') 
+        
+        file_name = new_file_name + '.txt'
+
+        file_path = os.path.join(dest_folder, file_name)
+
+
+
+
+
+
+        
 
         
