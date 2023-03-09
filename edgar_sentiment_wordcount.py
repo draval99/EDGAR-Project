b495e5d5995d8a251956f375aa418de731cf7570
Athul Parvelikudy <athulparvelikudy@kubrickgroup.com>
2023-03-09 11:46:10 +0000

    removing dead code from bottom

--- edgar_sentiment_wordcount.py
+++ edgar_sentiment_wordcount.py
@@ -58,12 +58,3 @@ def write_document_sentiments(input_folder : str, output_file : str):
         df.insert(0, 'report_type', file_name[1], inplace = True)
         df.insert(0, 'symbol', file_name[0], inplace = True)
         df.to_csv(file)
-
-    
-{
-    positive : [good, nice]
-
-}
-
-dict['positive'] == [good, nice]
-
